6bea05648e81e4e7ccc38f1b7ea05beb7213dc17
Sam Smith <wagner.godinho@gmail.com>
2021-04-29 06:37:05 -0300

    correção de bug no menu mobile e pequenas alterações

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-// Script main.js por | by Wagner Godinho Atualizado em | Update: 25/04/2021 
-
+// Script main.js por | by Wagner Godinho Atualizado em | Update: 29/04/2021 
 
 let i = 0; // Declara a variável 'i' e lhe atribui um valor | Declare the variable 'i' and assign a value 
 
-/* Cria uma função chamada toggleMenu, que ao ser chamada, dependendo do valor da variável 'i'
+/* A função chamada toggleMenu, ao ser chamada, dependendo do valor da variável 'i'
 	exibirá ou esconderá os itens do mobile menu. Funcionará como um interruptor. */
 
-/* Create a function toggleMenu, when called, depending the value of variable 'i', it will show
+/* The function toggleMenu, when called, depending the value of variable 'i', it will show
 	or hide the items of mobile menu. It will work as a switch */ 
 
 function toggleMenu(){
@@ -50,4 +49,19 @@ function toggleMenu(){
 		i = 0; // Altera valor da variavel 'i' para '0' | Changes variable 'i' to '0'
 	}
 	
+}
+
+/* Função resize, que ao ser chamada, oculta o menu mobile em telas com maiores resolução.
+   The function resize, when called, it hides the menu mobile on screens with higher resolution. */    
+
+function resize(){
+	if(screen.width >=768){
+		var x = document.getElementById("link_menu"); 
+		var y = document.getElementById("mobile_menu_itens");
+		x.style.backgroundImage = "url('img/menu.png')";
+		y.style.display = "none";
+		y.style.visibility = "hidden";
+		i = 0;
+
+	}
 }
